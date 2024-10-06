3aba9c4e484a096b0b02e6db3ec5c12061a3df3a
teddyyayo <teddy.ww@gmail.com>
2024-10-06 13:36:24 +0300

    Updates

--- SmartFarming/App.xaml.cs
+++ SmartFarming/App.xaml.cs
@@ -7,5 +7,13 @@
             InitializeComponent();
             MainPage = new MainPage();
         }
+        protected override Window CreateWindow(IActivationState activationState)
+        {
+            var window = base.CreateWindow(activationState);
+            window.Width = 460;
+            window.Height = 750;
+
+            return window;
+        }
     }
 }
