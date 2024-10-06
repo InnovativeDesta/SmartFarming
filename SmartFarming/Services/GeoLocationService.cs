3e21ffcdc58c3ec98f50702fda4e5a04207145f2
Tewodros Ambaw <tewodros.ambaw@hitachienergy.com>
2024-10-06 12:36:48 +0300

    Add Smart Farming App
    Add smart farming app

---
+++ SmartFarming/Services/GeoLocationService.cs
@@ -0,0 +1,32 @@
+﻿using Xamarin.Essentials;
+namespace SmartFarming
+{
+    public class GeolocationService
+    {
+        public async Task<Xamarin.Essentials.Location> GetLocationAsync()
+        {
+            try
+            {
+                // var location = await GeolocationService.GetLocationAsync();
+                var location = new Xamarin.Essentials.Location
+                {
+                    Latitude = 40.712776, // Example: Latitude for New York City
+                    Longitude = -74.005974 // Example: Longitude for New York City
+                };
+
+               /* location = await Xamarin.Essentials.Geolocation.GetLastKnownLocationAsync();
+                if (location == null)
+                {
+                    location = await Xamarin.Essentials.Geolocation.GetLocationAsync(new Xamarin.Essentials.GeolocationRequest(Xamarin.Essentials.GeolocationAccuracy.High));
+                }*/
+
+                return location;
+            }
+            catch (Exception ex)
+            {
+                // Handle location access errors
+                return null;
+            }
+        }
+    }
+}
---
+++ SmartFarming/Services/MeteomaticsApiService.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartFarming.Services
+{
+    using System;
+    using System.Net.Http;
+    using System.Net.Http.Headers;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class MeteomaticsApiService
+    {
+        private readonly string _username = "ahtiluoma_jarkko";
+        private readonly string _password = "AY1V0fc84b";
+        private readonly HttpClient _httpClient;
+
+        public MeteomaticsApiService()
+        {
+            _httpClient = new HttpClient();
+        }
+
+        public async Task<string> GetWeatherDataAsync()
+        {
+            // Encode the username and password into Base64
+            var credentials = Convert.ToBase64String(Encoding.ASCII.GetBytes($"{_username}:{_password}"));
+            string endpoint = $"https://{_username}:{_password}@api.meteomatics.com/2024-10-05T00:00:00ZP5D:PT1H/volumetric_soil_water_-5cm:m3m3,volumetric_soil_water_-15cm:m3m3,volumetric_soil_water_-50cm:m3m3,volumetric_soil_water_-150cm:m3m3/47.412164,9.340652/html?source=mix";
+             
+            // Add the Authorization header
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", credentials);
+
+            // Make the API request
+            HttpResponseMessage response = await _httpClient.GetAsync(endpoint);
+
+            if (response.IsSuccessStatusCode)
+            {
+                return await response.Content.ReadAsStringAsync();
+            }
+            else
+            {
+                throw new Exception($"API request failed with status code {response.StatusCode}");
+            }
+        }
+    }
+
+}
