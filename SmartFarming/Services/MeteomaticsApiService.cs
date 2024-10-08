90fa7396e93812ce8ad7a4008b31419e3dc3d84e
Tewodros W. Ambaw <teddy.ww@gmail.com>
2024-10-08 09:30:02 +0300

    Remove password

--- SmartFarming/Services/MeteomaticsApiService.cs
+++ SmartFarming/Services/MeteomaticsApiService.cs
@@ -14,8 +14,8 @@ namespace SmartFarming.Services
 
     public class MeteomaticsApiService
     {
-        private readonly string _username = "ahtiluoma_jarkko";
-        private readonly string _password = "AY1V0fc84b";
+        private readonly string _username;
+        private readonly string _password;
         private readonly HttpClient _httpClient;
 
         public MeteomaticsApiService()
@@ -26,8 +26,8 @@ namespace SmartFarming.Services
         public async Task<string> GetWeatherDataAsync()
         {
             // Encode the username and password into Base64
-            var credentials = Convert.ToBase64String(Encoding.ASCII.GetBytes($"{_username}:{_password}"));
-            string endpoint = $"https://{_username}:{_password}@api.meteomatics.com/2024-10-05T00:00:00ZP5D:PT1H/volumetric_soil_water_-5cm:m3m3,volumetric_soil_water_-15cm:m3m3,volumetric_soil_water_-50cm:m3m3,volumetric_soil_water_-150cm:m3m3/47.412164,9.340652/html?source=mix";
+            // var credentials = Convert.ToBase64String(Encoding.ASCII.GetBytes($"{_username}:{_password}"));
+            string endpoint = $"https://api.meteomatics.com/2024-10-05T00:00:00ZP5D:PT1H/volumetric_soil_water_-5cm:m3m3,volumetric_soil_water_-15cm:m3m3,volumetric_soil_water_-50cm:m3m3,volumetric_soil_water_-150cm:m3m3/47.412164,9.340652/html?source=mix";
              
             // Add the Authorization header
             _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", credentials);
